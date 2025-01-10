55e9f002c33f60e58b63b6cb3a979d7e49186d7c
Яков Харченко <user3000800@gmail.com>
2025-01-10 08:17:31 +0200

    last

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 
@@ -15,7 +16,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-        <App />
+        <Router>
+             <App />
+
+        </Router>
 
     </ThemeProvider>
 
